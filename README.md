87370d60fd254cea44144c665cdf90fbda8d107d
ilailsonrocha <ilailsonrocha@iec.gov.br>
2024-07-23 14:59:58 -0300

    Filtros... Matricula, nome, sobrenome

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 https://github.com/vsandrade/AspNetCoreWebAPI
 
 http://localhost:5000/swagger/smartschoolapi/swagger.json
+
+
+
+testando filtros
+http://localhost:5000/api/v1/aluno?ativo=1&nome=ma&matricula=1
+
+Testando filtros com paginação
+http://localhost:5000/api/v1/aluno?pageNumber=5&pageSize=2&ativo=1&nome=ma&matricula=1
+
+http://localhost:5000/api/v1/aluno?pageNumber=1&pageSize=10&ativo=1&nome=ma&matricula=5
--- SmartSchool.WebAPI/Data/Repository.cs
+++ SmartSchool.WebAPI/Data/Repository.cs
@@ -43,6 +43,21 @@ namespace SmartSchool.WebAPI.Data
             }
 
             query = query.AsNoTracking().OrderBy(o => o.Id);
+
+            if(!string.IsNullOrEmpty(pageParams.Nome))
+                query = query.Where(aluno => aluno.Nome
+                                                  .ToUpper()
+                                                  .Contains(pageParams.Nome.ToUpper()) || 
+                                             aluno.Sobrenome
+                                                  .ToUpper()
+                                                  .Contains(pageParams.Nome.ToUpper()));
+
+            if(pageParams.Matricula > 0)
+                query = query.Where(aluno => aluno.Matricula == pageParams.Matricula);
+
+            if(pageParams.Ativo != null)
+                query = query.Where(aluno => aluno.Ativo == (pageParams.Ativo != 0));
+
             
             // return await query.ToListAsync();
 
